def9ec6adb7f0f8f990eb18a213241039cd65e18
Thibaud Cordina <thibaud.cordina@gmail.com>
2018-10-17 11:13:00 +0200

    Liste données dans tableau GUI Qt

--- app.py
+++ app.py
@@ -7,15 +7,16 @@
 # WARNING! All changes made in this file will be lost!
 
 from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtWidgets import QTableWidgetItem
 
 from src.Controllers import ArticleController
-from src.Repositories import FournisseurRepository
+from src.Repositories import ArticleRepository, FournisseurRepository
 
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(710, 570)
+        MainWindow.resize(711, 570)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
         self.tabWidgetArticles = QtWidgets.QTabWidget(self.centralwidget)
@@ -29,15 +30,15 @@ class Ui_MainWindow(object):
         self.label = QtWidgets.QLabel(self.tab)
         self.label.setGeometry(QtCore.QRect(80, 414, 47, 13))
         self.label.setLayoutDirection(QtCore.Qt.LeftToRight)
-        self.label.setAlignment(QtCore.Qt.AlignRight | QtCore.Qt.AlignTrailing | QtCore.Qt.AlignVCenter)
+        self.label.setAlignment(QtCore.Qt.AlignRight|QtCore.Qt.AlignTrailing|QtCore.Qt.AlignVCenter)
         self.label.setObjectName("label")
         self.label_2 = QtWidgets.QLabel(self.tab)
         self.label_2.setGeometry(QtCore.QRect(80, 454, 47, 13))
-        self.label_2.setAlignment(QtCore.Qt.AlignRight | QtCore.Qt.AlignTrailing | QtCore.Qt.AlignVCenter)
+        self.label_2.setAlignment(QtCore.Qt.AlignRight|QtCore.Qt.AlignTrailing|QtCore.Qt.AlignVCenter)
         self.label_2.setObjectName("label_2")
         self.label_3 = QtWidgets.QLabel(self.tab)
         self.label_3.setGeometry(QtCore.QRect(363, 413, 47, 13))
-        self.label_3.setAlignment(QtCore.Qt.AlignRight | QtCore.Qt.AlignTrailing | QtCore.Qt.AlignVCenter)
+        self.label_3.setAlignment(QtCore.Qt.AlignRight|QtCore.Qt.AlignTrailing|QtCore.Qt.AlignVCenter)
         self.label_3.setObjectName("label_3")
         self.doubleSpinBoxArticlePrix = QtWidgets.QDoubleSpinBox(self.tab)
         self.doubleSpinBoxArticlePrix.setGeometry(QtCore.QRect(140, 450, 171, 22))
@@ -50,30 +51,44 @@ class Ui_MainWindow(object):
         self.pushButtonArticle.setObjectName("pushButtonArticle")
         self.label_4 = QtWidgets.QLabel(self.tab)
         self.label_4.setGeometry(QtCore.QRect(340, 450, 71, 20))
-        self.label_4.setAlignment(QtCore.Qt.AlignRight | QtCore.Qt.AlignTrailing | QtCore.Qt.AlignVCenter)
+        self.label_4.setAlignment(QtCore.Qt.AlignRight|QtCore.Qt.AlignTrailing|QtCore.Qt.AlignVCenter)
         self.label_4.setObjectName("label_4")
         self.comboBoxArticleFournisseur = QtWidgets.QComboBox(self.tab)
         self.comboBoxArticleFournisseur.setGeometry(QtCore.QRect(423, 449, 171, 22))
         self.comboBoxArticleFournisseur.setObjectName("comboBoxArticleFournisseur")
-        fournisseurs = FournisseurRepository.find_all()
-        for four in fournisseurs:
-            self.comboBoxArticleFournisseur.addItem(four[1], int(four[0]))
-        self.tableViewArticles = QtWidgets.QTableView(self.tab)
-        self.tableViewArticles.setGeometry(QtCore.QRect(20, 10, 661, 381))
-        self.tableViewArticles.setObjectName("tableViewArticles")
+        self.tableWidgetArticles = QtWidgets.QTableWidget(self.tab)
+        self.tableWidgetArticles.setGeometry(QtCore.QRect(7, 10, 691, 371))
+        self.tableWidgetArticles.setColumnCount(5)
+        self.tableWidgetArticles.setObjectName("tableWidgetArticles")
+        item = QtWidgets.QTableWidgetItem()
+        self.tableWidgetArticles.setHorizontalHeaderItem(0, item)
+        item = QtWidgets.QTableWidgetItem()
+        self.tableWidgetArticles.setHorizontalHeaderItem(1, item)
+        item = QtWidgets.QTableWidgetItem()
+        self.tableWidgetArticles.setHorizontalHeaderItem(2, item)
+        item = QtWidgets.QTableWidgetItem()
+        self.tableWidgetArticles.setHorizontalHeaderItem(3, item)
+        item = QtWidgets.QTableWidgetItem()
+        self.tableWidgetArticles.setHorizontalHeaderItem(4, item)
+        articles = ArticleRepository.find_all()
+        self.tableWidgetArticles.setRowCount(len(articles))
+        key = 0;
+        for article in articles:
+            for i in range(0,5):
+                item = QTableWidgetItem(str(article[i]))
+                item.setFlags(QtCore.Qt.ItemIsEnabled)
+                self.tableWidgetArticles.setItem(key, i, item)
+            key += 1
         self.tabWidgetArticles.addTab(self.tab, "")
         self.tab_2 = QtWidgets.QWidget()
         self.tab_2.setObjectName("tab_2")
         self.pushButtonFour = QtWidgets.QPushButton(self.tab_2)
         self.pushButtonFour.setGeometry(QtCore.QRect(320, 480, 75, 23))
         self.pushButtonFour.setObjectName("pushButtonFour")
-        self.tableViewFours = QtWidgets.QTableView(self.tab_2)
-        self.tableViewFours.setGeometry(QtCore.QRect(20, 10, 661, 421))
-        self.tableViewFours.setObjectName("tableViewFours")
         self.label_6 = QtWidgets.QLabel(self.tab_2)
         self.label_6.setGeometry(QtCore.QRect(110, 450, 47, 13))
         self.label_6.setLayoutDirection(QtCore.Qt.LeftToRight)
-        self.label_6.setAlignment(QtCore.Qt.AlignRight | QtCore.Qt.AlignTrailing | QtCore.Qt.AlignVCenter)
+        self.label_6.setAlignment(QtCore.Qt.AlignRight|QtCore.Qt.AlignTrailing|QtCore.Qt.AlignVCenter)
         self.label_6.setObjectName("label_6")
         self.lineEditFourName = QtWidgets.QLineEdit(self.tab_2)
         self.lineEditFourName.setGeometry(QtCore.QRect(170, 446, 171, 20))
@@ -84,8 +99,28 @@ class Ui_MainWindow(object):
         self.label_7 = QtWidgets.QLabel(self.tab_2)
         self.label_7.setGeometry(QtCore.QRect(360, 450, 47, 13))
         self.label_7.setLayoutDirection(QtCore.Qt.LeftToRight)
-        self.label_7.setAlignment(QtCore.Qt.AlignRight | QtCore.Qt.AlignTrailing | QtCore.Qt.AlignVCenter)
+        self.label_7.setAlignment(QtCore.Qt.AlignRight|QtCore.Qt.AlignTrailing|QtCore.Qt.AlignVCenter)
         self.label_7.setObjectName("label_7")
+        self.tableWidgetFours = QtWidgets.QTableWidget(self.tab_2)
+        self.tableWidgetFours.setGeometry(QtCore.QRect(7, 10, 690, 411))
+        self.tableWidgetFours.setObjectName("tableWidgetFours")
+        self.tableWidgetFours.setColumnCount(3)
+        self.tableWidgetFours.setRowCount(0)
+        item = QtWidgets.QTableWidgetItem()
+        self.tableWidgetFours.setHorizontalHeaderItem(0, item)
+        item = QtWidgets.QTableWidgetItem()
+        self.tableWidgetFours.setHorizontalHeaderItem(1, item)
+        item = QtWidgets.QTableWidgetItem()
+        self.tableWidgetFours.setHorizontalHeaderItem(2, item)
+        fours = FournisseurRepository.find_all()
+        self.tableWidgetFours.setRowCount(len(fours))
+        key = 0;
+        for four in fours:
+            for i in range(0, 3):
+                item = QTableWidgetItem(str(four[i]))
+                item.setFlags(QtCore.Qt.ItemIsEnabled)
+                self.tableWidgetFours.setItem(key, i, item)
+            key += 1
         self.tabWidgetArticles.addTab(self.tab_2, "")
         MainWindow.setCentralWidget(self.centralwidget)
         self.statusbar = QtWidgets.QStatusBar(MainWindow)
@@ -96,14 +131,6 @@ class Ui_MainWindow(object):
         self.tabWidgetArticles.setCurrentIndex(1)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
-        '''
-        self.tableViewArticles.setItem(0, 0, "ID")
-        self.tableViewArticles.setItem(0, 1, "Nom")
-        self.tableViewArticles.setItem(0, 2, "Prix")
-        self.tableViewArticles.setItem(0, 3, "Quantité")
-        self.tableViewArticles.setItem(0, 4, "fournisseur")
-        '''
-
         self.pushButtonArticle.clicked.connect(self.addArticle)
 
     def addArticle(self):
@@ -111,27 +138,42 @@ class Ui_MainWindow(object):
 
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
-        MainWindow.setWindowTitle(_translate("MainWindow", "Allo"))
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
         self.label.setText(_translate("MainWindow", "Nom"))
         self.label_2.setText(_translate("MainWindow", "Prix"))
         self.label_3.setText(_translate("MainWindow", "Quantité"))
         self.pushButtonArticle.setText(_translate("MainWindow", "Ajouter"))
         self.label_4.setText(_translate("MainWindow", "Fournisseur"))
-        self.tabWidgetArticles.setTabText(self.tabWidgetArticles.indexOf(self.tab),
-                                          _translate("MainWindow", "Articles"))
+        self.tableWidgetArticles.setSortingEnabled(True)
+        item = self.tableWidgetArticles.horizontalHeaderItem(0)
+        item.setText(_translate("MainWindow", "#"))
+        item = self.tableWidgetArticles.horizontalHeaderItem(1)
+        item.setText(_translate("MainWindow", "Nom"))
+        item = self.tableWidgetArticles.horizontalHeaderItem(2)
+        item.setText(_translate("MainWindow", "Prix (€)"))
+        item = self.tableWidgetArticles.horizontalHeaderItem(3)
+        item.setText(_translate("MainWindow", "Quantité"))
+        item = self.tableWidgetArticles.horizontalHeaderItem(4)
+        item.setText(_translate("MainWindow", "Fournisseur"))
+        self.tabWidgetArticles.setTabText(self.tabWidgetArticles.indexOf(self.tab), _translate("MainWindow", "Articles"))
         self.pushButtonFour.setText(_translate("MainWindow", "Ajouter"))
         self.label_6.setText(_translate("MainWindow", "Nom"))
         self.label_7.setText(_translate("MainWindow", "Ville"))
-        self.tabWidgetArticles.setTabText(self.tabWidgetArticles.indexOf(self.tab_2),
-                                          _translate("MainWindow", "Fournisseurs"))
+        item = self.tableWidgetFours.horizontalHeaderItem(0)
+        item.setText(_translate("MainWindow", "#"))
+        item = self.tableWidgetFours.horizontalHeaderItem(1)
+        item.setText(_translate("MainWindow", "Nom"))
+        item = self.tableWidgetFours.horizontalHeaderItem(2)
+        item.setText(_translate("MainWindow", "Ville"))
+        self.tabWidgetArticles.setTabText(self.tabWidgetArticles.indexOf(self.tab_2), _translate("MainWindow", "Fournisseurs"))
 
 
 if __name__ == "__main__":
     import sys
-
     app = QtWidgets.QApplication(sys.argv)
     MainWindow = QtWidgets.QMainWindow()
     ui = Ui_MainWindow()
     ui.setupUi(MainWindow)
     MainWindow.show()
     sys.exit(app.exec_())
+
